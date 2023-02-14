d5bc92d0f4ee2be291a443d6840ba9418e582be4
정택원 <chtw2001@taekwonMacBookPro.local>
2023-02-14 23:57:30 +0900

    유저 상세조회 페이지 작성

--- account/urls.py
+++ account/urls.py
@@ -6,6 +6,7 @@ from rest_framework import urls
 urlpatterns = [
     path('signup/', views.UserCreate.as_view()),
     path('signup/<int:pk>/', views.UserDetail.as_view()), # /pk 들어가면 회원 detail(수정, 삭제)
+    path('user/<int:pk>/', views.UserPage.as_view()),
     # path('api-auth/', include('rest_framework.urls')), # superuser로 로그인하면 404에러뜸 그래서 주석처리
 ]
 
