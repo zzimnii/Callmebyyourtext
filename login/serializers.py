caddfe6f110a16a027e24086abfb155b17ca70f2
chtw2001 <jungtaekwon1019@gmail.com>
2023-02-17 16:13:56 +0900

    add comments...

--- login/serializers.py
+++ login/serializers.py
@@ -9,6 +9,8 @@ from django.contrib.auth import authenticate
 
 from .models import Profile
 
+# Username을 받지 않는데 어떻게 항목에 받는지? 단순히 fields에 적어놓는다는 이유로? 
+# username에 대한 자료형도 선언하지 않았는데? 또한 ModelSerializer에 username이라는 항목도 없음...
 class RegisterSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
         required=True,
@@ -42,6 +44,9 @@ class RegisterSerializer(serializers.ModelSerializer):
         user.save()
         token = Token.objects.create(user=user)
         return user
+    # User모델이 만들어질때 username은 unique=False로 만들고싶음
+    # 로그아웃또한 구현하고 싶음
+
     
 class LoginSerializer(serializers.ModelSerializer):
     username = serializers.CharField(required=True)
@@ -61,3 +66,6 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ('point', 'image')
+# Profile모델 뿐만 아니라 User모델의 항목도 함께 보여주고 싶음
+# => Profile모델에 User모델의 속성을 ForiegnKey를 이용하여 연결한 후
+# Profile 모델만 보여주기
