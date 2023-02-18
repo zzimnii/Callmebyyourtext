bc22257a71dee24a7a4c68539ff6aa5923af08c5
zzimnii <happykeil@naver.com>
2023-02-18 14:43:35 +0900

    signup retry

--- postapp/serializers.py
+++ postapp/serializers.py
@@ -1,21 +1,22 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, ReadOnlyField
 from .models import Question, Comment
-
+from login.models import User
 
 class CommentSerializer(ModelSerializer):
-    #user = serializers.ReadOnlyField(source = 'user.nickname')
+    # comment_writer = ReadOnlyField(source='User.username')
+    # user = serializers.ReadOnlyField(source = 'user.nickname')
     class Meta:
         model = Comment
         fields = ['id', 'comment', 'comment_writer', 'question']
 
 class QuestionSerializer(ModelSerializer):
-    #user = serializers.ReadOnlyField(source = 'user.nickname')
+    # question_writer = ReadOnlyField(source='Profile.user.username')
     class Meta:
         model = Question
         fields = ['id', 'question', 'question_writer']
 
 class QuestionDetailSerializer(ModelSerializer):
-    #user = serializers.ReadOnlyField(source = 'user.nickname')
+    # question_writer = ReadOnlyField(source='User.username')
     comments = CommentSerializer(many=True, read_only=True)
 
     class Meta:
