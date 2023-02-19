ca7b911de0c1f3f6cf11e969dbc5a444f29b0610
chtw2001 <jungtaekwon1019@gmail.com>
2023-02-19 19:08:44 +0900

    password1 => password, last_login didnt work

--- login/models.py
+++ login/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 class UserManager(BaseUserManager):
     # 일반 user 생성
-    def create_user(self, email, name, password1=None):#  nickname, 
+    def create_user(self, email, name, password=None):#  nickname, 
         if not email:
             raise ValueError('must have user email')
         if not name:
@@ -15,14 +15,14 @@ class UserManager(BaseUserManager):
             name = name,
             # nickname = nickname,
         )
-        user.set_password(password1)
+        user.set_password(password)
         user.save(using=self._db)
         return user
 
     # 관리자 user 생성
     def create_superuser(self, email, name, password=None):
         user = self.create_user(
-            email,
+            email = email,
             password = password,
             name = name
         )
@@ -40,7 +40,23 @@ class User(AbstractBaseUser):
     # User 모델의 필수 field
     is_active = models.BooleanField(default=True)    
     is_admin = models.BooleanField(default=False)
-    
+    @property
+    def is_superuser(self):
+        return self.is_admin
+
+    @property
+    def is_staff(self):
+       return self.is_admin
+
+    def has_perm(self, perm, obj=None):
+       return self.is_admin
+
+    def has_module_perms(self, app_label):
+       return self.is_admin
+
+    @is_staff.setter
+    def is_staff(self, value):
+        self._is_staff = value
     # 헬퍼 클래스 사용
     objects = UserManager()
 
