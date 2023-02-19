d96ff17da244c662aaddfcaea10a07d812904f1e
chtw2001 <jungtaekwon1019@gmail.com>
2023-02-19 15:40:37 +0900

    login => return token

--- login/urls.py
+++ login/urls.py
@@ -6,7 +6,10 @@ urlpatterns = [
     # path('login/', LoginView.as_view()),
     # path('profile/<int:pk>/', ProfileView.as_view()),
     path('signup/', UserCreate.as_view()),
+    path('login/', LoginView.as_view()),
+    path('profile/', ProfileList.as_view()), # <int:pk> 써서 해당 계정 정보만 보여주게 해야함
+                                             # => 권한이 있는지 확인 후 보여주기 or 안보여주기
     path('', include('rest_framework.urls')),
-    path('info/', ProfileList.as_view()),
-    path('re-login/', LoginView.as_view()),
+    
+    
 ]
