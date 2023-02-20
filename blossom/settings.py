f44b3f6739fd3f95f47497920d7d22fbe2c3ba6e
chtw2001 <jungtaekwon1019@gmail.com>
2023-02-21 00:13:01 +0900

    test

--- blossom/settings.py
+++ blossom/settings.py
@@ -125,10 +125,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-REST_FRAMEWORK = {
-	'DEFAULT_AUTHENTICATION_CLASSES':
-    ['rest_framework.authentication.TokenAuthentication', ],
-    }
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
