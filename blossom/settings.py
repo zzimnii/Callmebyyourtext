805de20d1001365e0dc24f9bd3741c2f1feca326
chtw2001 <jungtaekwon1019@gmail.com>
2023-02-23 16:32:37 +0900

    authenticate by token

--- blossom/settings.py
+++ blossom/settings.py
@@ -39,22 +39,22 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
 	'DEFAULT_AUTHENTICATION_CLASSES': [
-    'rest_framework.authentication.SessionAuthentication',
+    # 'rest_framework.authentication.SessionAuthentication',
     'rest_framework.authentication.TokenAuthentication', 
     ],
     'DEFAULT_PERMISSION_CLASSES': [
    'rest_framework.permissions.AllowAny',],
 
-    'DEFAULT_RENDERER_CLASSES': [
-    'djangorestframework_camel_case.render.CamelCaseJSONRenderer', 
-    'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
-    ],
+    # 'DEFAULT_RENDERER_CLASSES': [
+    # 'djangorestframework_camel_case.render.CamelCaseJSONRenderer', 
+    # 'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
+    # ],
 
-    'DEFAULT_PARSER_CLASSES': [
-    'djangorestframework_camel_case.parser.CamelCaseFormParser', 
-    'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
-    'djangorestframework_camel_case.parser.CamelCaseJSONParser',
-    ],
+    # 'DEFAULT_PARSER_CLASSES': [
+    # 'djangorestframework_camel_case.parser.CamelCaseFormParser', 
+    # 'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
+    # 'djangorestframework_camel_case.parser.CamelCaseJSONParser',
+    # ],
 }
 
 MIDDLEWARE = [
