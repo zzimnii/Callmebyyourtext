201868d912122900db53af6567a6425220d16f81
zzimnii <102315673+zzimnii@users.noreply.github.com>
2023-02-25 10:06:24 +0900

    Update serializers.py

--- postapp/serializers.py
+++ postapp/serializers.py
@@ -2,26 +2,28 @@ from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer, ReadOnlyField
 from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
 from .models import Question, Comment
+from django.conf import settings
+from datetime import datetime
 
 class CommentSerializer(ModelSerializer):
     writer = serializers.ReadOnlyField(source = 'writer.name')
     class Meta:
         model = Comment
-        fields = ['id','comment', 'question', 'writer', 'anonymous', 'created_at', 'opened']
+        fields = ['id','comment', 'questionId', 'writer', 'anonymous', 'created_at']
 
 
 class CommentCreateSerializer(ModelSerializer):
     permission_classes = [AllowAny]
     class Meta:
         model = Comment
-        fields = ['id', 'question', 'comment', 'anonymous', 'created_at']
+        fields = ['id', 'questionId', 'comment', 'anonymous', 'created_at']
 
     def create(self, validated_data):
         return Comment.objects.create(
             comment=validated_data['comment'],
             writer=validated_data['writer'],
             anonymous=validated_data['anonymous'],
-            question=validated_data['question']
+            questionId=validated_data['questionId']
             )
 
 
