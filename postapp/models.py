600f3e4f5b39d4c1076e851e5052e518de3550a3
zzimnii <102315673+zzimnii@users.noreply.github.com>
2023-02-25 09:56:10 +0900

    Update models.py

--- postapp/models.py
+++ postapp/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 from login.models import User
-from django.conf import settings
-from django.utils import timezone
 
 class Question(models.Model):
+    id = models.AutoField(primary_key=True)
     question = models.TextField()
     writer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='question', null = True) #질문 작성자
     created_at = models.DateTimeField(auto_now_add=True, null=True)
@@ -13,7 +12,8 @@ class Question(models.Model):
 
 
 class Comment(models.Model):
-    questionId = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="comments")     #질문 ID
+    id = models.AutoField(primary_key=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="comments")     #질문 ID
     comment = models.TextField()
     writer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments', null = True, blank=True)
     anonymous = models.BooleanField(default=True, blank=True, null=True)
@@ -21,8 +21,3 @@ class Comment(models.Model):
     
     def __str__(self):
         return self.comment
-
-class OpenComment(models.Model):
-    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='openComments')
-    reader = models.ForeignKey(User, on_delete=models.CASCADE, related_name='openComments')
-    is_seen = models.BooleanField(default=False)
