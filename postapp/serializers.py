240888a821b86b05e771494c0f3bb0c9846ec704
zzimnii <102315673+zzimnii@users.noreply.github.com>
2023-02-25 09:56:40 +0900

    Update serializers.py

--- postapp/serializers.py
+++ postapp/serializers.py
@@ -1,35 +1,27 @@
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer, ReadOnlyField
 from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
-from .models import Question, Comment, OpenComment
-from django.conf import settings
-from datetime import datetime
+from .models import Question, Comment
 
 class CommentSerializer(ModelSerializer):
     writer = serializers.ReadOnlyField(source = 'writer.name')
     class Meta:
         model = Comment
-        fields = ['id','comment', 'questionId', 'writer', 'anonymous', 'created_at']
+        fields = ['id','comment', 'question', 'writer', 'anonymous', 'created_at', 'opened']
 
-class OpenCommentSerializer(ModelSerializer):
-    class Meta:
-        model = OpenComment
-        fields = ['comment', 'reader']
 
 class CommentCreateSerializer(ModelSerializer):
     permission_classes = [AllowAny]
     class Meta:
         model = Comment
-        fields = ['id', 'questionId', 'comment', 'anonymous', 'created_at']
+        fields = ['id', 'question', 'comment', 'anonymous', 'created_at']
 
     def create(self, validated_data):
-        x = datetime.now()
         return Comment.objects.create(
-            id = str(x.year)+str(x.month)+str(x.day)+str(x.hour)+str(x.minute)+str(x.second)+str(x.microsecond),
             comment=validated_data['comment'],
             writer=validated_data['writer'],
             anonymous=validated_data['anonymous'],
-            questionId=validated_data['questionId']
+            question=validated_data['question']
             )
 
 
@@ -40,9 +32,7 @@ class QuestionSerializer(ModelSerializer):
         fields = ['id', 'question', 'writer', 'created_at']
 
     def create(self, validated_data):
-        x = datetime.now()
         return Question.objects.create(
-            id = str(x.year)+str(x.month)+str(x.day)+str(x.hour)+str(x.minute)+str(x.second)+str(x.microsecond),
             writer=validated_data['writer'],
             question=validated_data['question']
             )
